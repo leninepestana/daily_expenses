7d3de32808f8a4ea8245c9b29be505733cee4e98
leninepestana <leninepestana@gmail.com>
2022-09-10 19:28:38 +0100

    Refactoring form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,26 +4,33 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const [createExpense, setCreateExpense] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
     console.log(expenseData);
     props.onAddNewExpense(expenseData);
-    setCreateExpense(false);
+    setIsEditing(false);
   };
 
-  const createExpenseHandler = () => {
-    setCreateExpense(true);
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      {!createExpense && (
-        <button onClick={createExpenseHandler}>Add New Expense</button>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
       )}
-      {createExpense && (
-        <ExpenseForm onCancel onSaveExpenseData={saveExpenseDataHandler} />
+      {isEditing && (
+        <ExpenseForm
+          onCancel={stopEditingHandler}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
       )}
     </div>
   );
